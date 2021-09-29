d1ca865cb0a1abb03188f35a9860bdbcfa022dfe
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-09-29 20:34:51 +0530

    ADDED : Code for Successful Unparking

--- src/test/java/com/parkingLot/ParkingLotSystemTest.java
+++ src/test/java/com/parkingLot/ParkingLotSystemTest.java
@@ -1,15 +1,30 @@
 package com.parkingLot;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ParkingLotSystemTest {
 
+    Object vehicle;
+    ParkingLotSystem parkingLotSystem;
+
+    @BeforeEach
+    void setUp() {
+        vehicle = new Object();
+        parkingLotSystem = new ParkingLotSystem();
+    }
+
     @Test
     void givenAVehicle_WhenParked_ShouldReturnTrue() {
-        Object vehicle = new Object();
-        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
         boolean isParked = parkingLotSystem.park(vehicle);
         Assertions.assertTrue(isParked);
+    }
+
+    @Test
+    void givenAVehicle_WhenUnParked_ShouldReturnTrue() {
+        parkingLotSystem.park(vehicle);
+        boolean isUnParked = parkingLotSystem.unPark(vehicle);
+        Assertions.assertTrue(isUnParked);
     }
 }
